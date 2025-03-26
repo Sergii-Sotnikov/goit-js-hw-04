db63c64bd57f882b3c48347274ca48dcdf2f0109
SotnikovSS <sotnikovss90@gmail.com>
2025-03-26 11:42:20 +0100

    UPDT

--- js/task-1.js
+++ js/task-1.js
@@ -1,15 +1,18 @@
 console.log("<<<<<<<<<<<<<<<<TASK 1>>>>>>>>>>>>>>>");
-function slugify(title) {
-  title = title.toLowerCase();
-  const array = title.split(" ");
-  const slug = array.join("-");
-  return slug;
+function isEnoughCapacity(products, containerSize) {
+  let array;
+  array = Object.values(products);
+  let total = 0;
+  for (const quantityOfGoods of array) {
+    total += quantityOfGoods;
+  }
+  return total <= containerSize;
 }
 
-console.log(slugify("Arrays for beginners")); // "arrays-for-beginners"
-console.log(slugify("English for developer")); // "english-for-developer"
-console.log(slugify("Ten secrets of JavaScript")); // "ten-secrets-of-javascript"
-console.log(slugify("How to become a JUNIOR developer in TWO WEEKS")); // "how-to-become-a-junior-developer-in-two-weeks"
+console.log(isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8));
+console.log(isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12));
+console.log(isEnoughCapacity({ apples: 1, lime: 5, tomatoes: 3 }, 14));
+console.log(isEnoughCapacity({ apples: 18, potatoes: 5, oranges: 2 }, 7));
 
 console.log("");
 console.log("");
--- js/task-3.js
+++ js/task-3.js
@@ -1,19 +1,24 @@
 console.log("<<<<<<<<<<<<<<<<TASK 3>>>>>>>>>>>>>>>");
-function filterArray(numbers, value) {
-  const newArray = [];
-  for (const a of numbers) {
-    if (a > value) {
-      newArray.push(a);
-    }
-  }
-  return newArray;
-}
+const profile = {
+  username: "Jacob",
+  playTime: 300,
+  getInfo() {
+    return `${profile.username} has ${profile.playTime} active hours!`;
+  },
+  changeUsername(newName) {
+    this.username = newName;
+  },
+  updatePlayTime(hours) {
+    this.playTime = this.playTime + hours;
+  },
+};
+console.log(profile.getInfo());
 
-console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
-console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
-console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
-console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
-console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
+profile.changeUsername("Marco");
+console.log(profile.getInfo());
+
+profile.updatePlayTime(20);
+console.log(profile.getInfo());
 
 console.log("");
 console.log("");
